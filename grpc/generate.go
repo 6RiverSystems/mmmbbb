334afc4f65770db0db702c703af411386c37c712
Matthew Gabeler-Lee <mgabeler-lee@6river.com>
2021-11-29 20:23:51 -0500

    fix: prevent genproto from being tidied away

--- grpc/generate.go
+++ grpc/generate.go
@@ -26,6 +26,7 @@ package grpc
 import (
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
+	_ "google.golang.org/genproto/googleapis/pubsub/v1"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
