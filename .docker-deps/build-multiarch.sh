6de355d4dffa826b572bf4a987e3274601300b45
Matthew Gabeler-Lee <mgabeler-lee@6river.com>
2022-03-10 00:10:36 -0500

    fix: trying other things

--- .docker-deps/build-multiarch.sh
+++ .docker-deps/build-multiarch.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Copyright (c) 2022 6 River Systems
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 # TODO: replace this and the makefile with a magefile
 
 set -xeuo pipefail
@@ -35,6 +54,10 @@ done
 platforms="${platforms[*]}"
 platforms="${platforms// /,}"
 builderargs=(--builder mmmbbb-multiarch)
+ctxargs=()
+if [ "${CI}" ]; then
+	ctxargs+=(--context mmmbbb-multiarch)
+fi
 for bin in "${BINARY_NAMES}" ; do
 	basetag="mmmbbb-${bin}:$(<.version)"
 	tagargs=(-t "$basetag")
@@ -46,6 +69,7 @@ for bin in "${BINARY_NAMES}" ; do
 		tagargs+=(-t "6river/${basetag}")
 	fi
 	BINARYNAME=bin docker \
+		"${ctxargs[@]}" \
 		buildx build \
 		"${builderargs[@]}" \
 		--platform "${platforms}" \
