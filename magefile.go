3f37e68d353e331513a3b466cde1b3c802d093ce
Matthew Gabeler-Lee <mgabeler-lee@6river.com>
2023-10-04 20:35:09 -0400

    fix: temporarily disable dockerhub push

--- magefile.go
+++ magefile.go
@@ -885,12 +885,13 @@ func dockerRunMultiArch(ctx context.Context, cmd string, mode string, arches ...
 			// push latest tag on main
 			args = append(args, "-t", gcrBase+baseImage+":latest")
 			args = append(args, "-t", arBase+baseImage+":latest")
-			// also push to docker hub for builds on main
-			if os.Getenv("DOCKERHUB_USER") != "" {
-				mg.CtxDeps(ctx, Docker{}.HubLogin)
-				args = append(args, "-t", "6river/"+baseTag)
-				args = append(args, "-t", "6river/"+baseImage+":latest")
-			}
+			// TODO: why is dockerhub access broken? CI secrets expired?
+			// // also push to docker hub for builds on main
+			// if os.Getenv("DOCKERHUB_USER") != "" {
+			// 	mg.CtxDeps(ctx, Docker{}.HubLogin)
+			// 	args = append(args, "-t", "6river/"+baseTag)
+			// 	args = append(args, "-t", "6river/"+baseImage+":latest")
+			// }
 		}
 	} else {
 		// base tag is not valid to push, but a useful local thing to use for the
